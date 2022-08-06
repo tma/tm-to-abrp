3909476b39fcd79362c725b1c6d85172ab0d61f0
Thomas Maurer <tma@github.com>
2022-08-06 10:57:28 +0000

    Use sync.Map

--- internal/app/http.go
+++ internal/app/http.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"text/template"
 	"time"
+	"fmt"
 
 	"github.com/gorilla/mux"
 )
@@ -15,8 +16,19 @@ var indexTemplate = template.Must(template.ParseFiles("web/templates/index.html"
 
 func indexHandler(car *Car) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		carTmData, _ := json.MarshalIndent(car.tmData, "", "  ")
-		carAbrpData, _ := json.MarshalIndent(car.abrpData, "", "  ")
+		tmDataMap := map[string]interface{}{}
+		car.tmData.Range(func(key, value interface{}) bool {
+			tmDataMap[fmt.Sprint(key)] = value
+			return true
+		})
+		carTmData, _ := json.MarshalIndent(tmDataMap, "", "  ")
+
+		abrpDataMap := map[string]interface{}{}
+		car.abrpData.Range(func(key, value interface{}) bool {
+			abrpDataMap[fmt.Sprint(key)] = value
+			return true
+		})
+		carAbrpData, _ := json.MarshalIndent(abrpDataMap, "", "  ")
 
 		var carAbrpSendContinuousButtonText string
 		if car.abrpSendActive {
